33d560ed44a2b04d2a2269ef32a0f766902ce6c7
meuus90 <meu12002@gmail.com>
2019-06-22 16:12:56 +0900

    First Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component, Fragment } from "react"
+import { createPortal } from "react-dom"
+
+class Portals extends Component {
+    render() {
+        return createPortal(
+            <Message />, document.getElementById("touchme")
+        )
+    }
+}
+const Message = () => "Just touched it !"
+
+class ReturnTypes extends Component {
+    render() {
+        return "Hello!!"
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <ReturnTypes />
+                <Portals />
+            </Fragment>
+        )
+    }
+}
+
+export default App
