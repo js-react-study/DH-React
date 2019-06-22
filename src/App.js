cf30a8ab04113759b7de4144bb5bdf2c31509d1e
meuus90 <meu12002@gmail.com>
2019-06-22 17:26:14 +0900

    Add Protecting Error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,46 @@
 import React, { Component, Fragment } from "react"
 import { createPortal } from "react-dom"
 
+const BoundaryHOC = ProtectedComponent =>
+    class Boundary extends Component {
+        state = {
+            hasError: false
+        }
+        componentDidCatch = () => {
+            this.setState({
+                hasError: true
+            })
+        }
+        render() {
+            const { hasError } = this.state
+            if (hasError) {
+                return <ErrorFallback />
+            } else {
+                return <ProtectedComponent />
+            }
+        }
+    }
+
+class ErrorMaker extends Component {
+    state = {
+        friends: ["a", "b", "c"]
+    }
+
+    componentDidMount = () => {
+        setTimeout(() => {
+            this.setState({
+                friends: undefined
+            })
+        }, 2000)
+    }
+    render() {
+        const { friends } = this.state
+        return friends.map(friend => ` ${friend} `)
+    }
+}
+
+const PErrorMaker = BoundaryHOC(ErrorMaker)
+
 class Portals extends Component {
     render() {
         return createPortal(
@@ -8,6 +48,9 @@ class Portals extends Component {
         )
     }
 }
+
+const PPortals = BoundaryHOC(Portals)
+
 const Message = () => "Just touched it !"
 
 class ReturnTypes extends Component {
@@ -16,15 +59,18 @@ class ReturnTypes extends Component {
     }
 }
 
+const ErrorFallback = () => " Sorry something went wrong"
+
 class App extends Component {
     render() {
         return (
             <Fragment>
                 <ReturnTypes />
-                <Portals />
+                <PPortals />
+                <PErrorMaker />
             </Fragment>
         )
     }
 }
 
-export default App
+export default BoundaryHOC(App)
